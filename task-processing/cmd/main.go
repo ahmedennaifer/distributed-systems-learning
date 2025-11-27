fb1ae7645aa744ed400cd37ae6dc77a98a15bf4b
AhmedEnnaifer <ahmedennaifer00@gmail.com>
2025-11-27 21:32:22 +0100

    feat: docker

--- task-processing/cmd/main.go
+++ task-processing/cmd/main.go
@@ -5,26 +5,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
+	"github.com/ahmedennaifer/taskq/internal"
 	"github.com/google/uuid"
 )
 
 var cache = NewCache()
 
 type Cache struct {
-	Db map[uuid.UUID]Task
+	Db map[uuid.UUID]internal.Task
 }
 
 func NewCache() *Cache {
 	return &Cache{
-		Db: make(map[uuid.UUID]Task, 0),
+		Db: make(map[uuid.UUID]internal.Task, 0),
 	}
 }
 
-func (c *Cache) Add(task Task) error {
+func (c *Cache) Add(task internal.Task) error {
 	for _, ctask := range c.Db {
-		if ctask.Id == task.Id {
+		if ctask.Name == task.Name {
 			fmt.Printf("error: cannot add task %v, it already exists\n", task.Name)
 			return fmt.Errorf("error: cannot add task %v, it already exists", task.Name)
 		}
@@ -33,40 +33,8 @@ func (c *Cache) Add(task Task) error {
 	return nil
 }
 
-type Task struct {
-	Id         uuid.UUID `json:"id"`
-	Name       string    `json:"name"`
-	Type       string    `json:"type"`
-	Body       string    `json:"body"`
-	CreatedAt  time.Time `json:"created_at"`
-	FinishedAt time.Time `json:"finished_at"`
-}
-
-func NewTask() *Task {
-	return &Task{
-		Id: uuid.New(),
-	}
-}
-
-func fieldEmptyError(field string) error {
-	return fmt.Errorf("error: field %v cannot be empty", field)
-}
-
-func (t *Task) Validate() error {
-	if t.Name == "" {
-		return fieldEmptyError("name")
-	}
-	if t.Type == "" {
-		return fieldEmptyError("type")
-	}
-	if t.Body == "" {
-		return fieldEmptyError("body")
-	}
-	return nil
-}
-
 func handlePost(w http.ResponseWriter, r *http.Request) {
-	task := NewTask()
+	task := internal.NewTask()
 	if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
 		fmt.Printf("error decoding payload: %v\n", err)
 		http.Error(w, fmt.Sprintf("error: cannot decode payload: %v", err), 500)
@@ -75,6 +43,7 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 	if err := task.Validate(); err != nil {
 		fmt.Printf("%v\n", err)
 		http.Error(w, fmt.Sprintf("%v\n", err), 500)
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	if err := cache.Add(*task); err != nil {
---
+++ task-processing/internal/task.go
@@ -0,0 +1,41 @@
+package internal
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Task struct {
+	Id         uuid.UUID      `json:"id"`
+	Name       string         `json:"name"`
+	Type       string         `json:"type"`
+	Params     map[string]any `json:"body"`
+	CreatedAt  time.Time      `json:"created_at"`
+	FinishedAt time.Time      `json:"finished_at"`
+}
+
+func NewTask() *Task {
+	return &Task{
+		Id:        uuid.New(),
+		CreatedAt: time.Now(),
+	}
+}
+
+func fieldEmptyError(field string) error {
+	return fmt.Errorf("error: field %v cannot be empty", field)
+}
+
+func (t *Task) Validate() error {
+	if t.Name == "" {
+		return fieldEmptyError("name")
+	}
+	if t.Type == "" {
+		return fieldEmptyError("type")
+	}
+	if len(t.Params) == 0 {
+		return fieldEmptyError("body")
+	}
+	return nil
+}
